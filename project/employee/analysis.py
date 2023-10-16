e27ee53ccd1a4f1102386001d195a67d848ebb25
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2023-10-16 08:16:53 +0530

    handle null values in analysis
    Signed-off-by: Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>

--- project/employee/analysis.py
+++ project/employee/analysis.py
@@ -6,23 +6,18 @@ from django.http import HttpResponse, JsonResponse
 
 
 
-########### Count Records ##############
 def count_records(self):
-    # Get a queryset for the model
     queryset = Employee.objects.all()
 
-    # Use the count() method to find the total number of data points
     total_data_points = queryset.count()
-    print(total_data_points)
     data = {
         'total_data_points': total_data_points
     }
     return JsonResponse(data)
 
-############# Mean #############
 def find_mean(self):
-    # Calculate the average salary
-    average_salary = Employee.objects.aggregate(Avg('prevailing_wage'))
+    # Calculate the average salary excluding empty values
+    average_salary = Employee.objects.exclude(prevailing_wage__isnull=True).aggregate(Avg('prevailing_wage'))
 
     # The result is a dictionary with the average value
     average_salary_value = average_salary['prevailing_wage__avg']
@@ -32,16 +27,15 @@ def find_mean(self):
     }
     return JsonResponse(data)
 
-
-############ Median ###########
 def median(self):
-    # Your raw SQL query to calculate the median
+    # Your modified raw SQL query to calculate the median excluding empty values
     query = """
         SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY prevailing_wage) AS median
-        FROM employee_Employee;
+        FROM employee_Employee
+        WHERE prevailing_wage IS NOT NULL;
     """
 
-    # Execute the raw SQL query
+    # Execute the modified raw SQL query
     with connection.cursor() as cursor:
         cursor.execute(query)
         result = cursor.fetchone()
@@ -54,15 +48,15 @@ def median(self):
     return JsonResponse(data)
 
 
-########### 25 percentile ################
 def percentile_25(self):
-    # Your raw SQL query to calculate the 25th percentile
+    # Your modified raw SQL query to calculate the 25th percentile excluding empty values
     query = f"""
         SELECT percentile_cont(0.25) WITHIN GROUP (ORDER BY prevailing_wage) AS percentile_25
-        FROM employee_Employee;
+        FROM employee_Employee
+        WHERE prevailing_wage IS NOT NULL;
     """
 
-    # Execute the raw SQL query
+    # Execute the modified raw SQL query
     with connection.cursor() as cursor:
         cursor.execute(query)
         result = cursor.fetchone()
@@ -74,16 +68,15 @@ def percentile_25(self):
     }
     return JsonResponse(data)
 
-
-######## 75 percentile #########
 def percentile_75(self):
-    # Your raw SQL query to calculate the 25th percentile
+    # Your modified raw SQL query to calculate the 75th percentile excluding empty values
     query = """
         SELECT percentile_cont(0.75) WITHIN GROUP (ORDER BY prevailing_wage) AS percentile_75
-        FROM employee_Employee;
+        FROM employee_Employee
+        WHERE prevailing_wage IS NOT NULL;
     """
 
-    # Execute the raw SQL query
+    # Execute the modified raw SQL query
     with connection.cursor() as cursor:
         cursor.execute(query)
         result = cursor.fetchone()
